b63aa2aea1910823ada7d87c0944f11360d28e54
codex-mohan <codexmohan@gmail.com>
2024-09-24 21:03:36 +0530

    Massive changes. Recreated the app, used tailwind-css colors directly instead of css, added tw-colors for theme management, moved the main ui to frontend folder, backend in the api folder, setup poetry for the python backend project.

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import SplashScreen from "@/components/SplashScreen";
+
+export default function Home() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <SplashScreen />
+    </div>
+  );
+}
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,56 @@
+import type { Config } from "tailwindcss";
+import { createThemes } from "tw-colors";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [
+    require("tailwindcss-animate"),
+    createThemes({
+      light: {
+        primary: "steelblue",
+        secondary: "darkblue",
+      },
+    }),
+  ],
+};
+export default config;
